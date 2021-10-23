a458764d78ea7af6b74c469ef82b6aae6c2edfd3
manegit <m1n@gmx.net>
2021-10-23 07:37:25 +0000

    add

--- save.sh
+++ save.sh
@@ -1,2 +1,3 @@
-docker image save -o ../docker-ubuntu-custom.tar mane1docker/ubuntu-custom:latest
-gzip ../docker-ubuntu-custom.tar
+docker image save -o /tmp/docker-ubuntu-custom.tar mane1docker/ubuntu-custom:latest
+gzip /tmp/docker-ubuntu-custom.tar
+mv -f /tmp/docker-ubuntu-custom.tar.gz ~/
